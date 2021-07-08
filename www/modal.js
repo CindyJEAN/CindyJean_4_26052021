fe3100aa2a914c8a4a66085bbc4a93986bf062ac
Cindy Jean <cindy.jean83@gmail.com>
2021-07-08 13:49:20 +0200

    Modifications sur les boutons et fermeture de la modale

--- www/modal.js
+++ www/modal.js
@@ -1,5 +1,5 @@
 /**
- * [editNav description]
+ * [editNav description] ????
  *
  * @return  {[type]}  [return description]
  */
@@ -14,27 +14,69 @@ function editNav() {
 
 // DOM Elements
 /**
- * le fond de la modale 
+ * le fond de la modale
  *
- * @type   {HTMLElement}  .bground  [.bground description]
+ * @type   {HTMLElement}  .bground
  */
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+/**
+ * la section hero
+ *
+ * @type   {HTMLElement}  .hero-section
+ */
+const heroSection = document.querySelector(".hero-section");
+/**
+ * le footer
+ *
+ * @type   {HTMLElement}  .footer
+ */
+const footer = document.querySelector("footer");
+/**
+ * le bouton close
+ *
+ * @type   {HTMLElement}  .close
+ */
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+//close modal event
+closeBtn.addEventListener("click", closeModal);
+
 // launch modal form
 /**
- * [launchModal description]
+ * Au lancement de la modale, on fait apparaître la modale et disparaître la 
+ * section hero et le footer, pour ne pas que l'on voit le fond si on swipe vers le bas
  *
- * @return  {[type]}  [return description]
+ * @return  {Boolean}
  */
 function launchModal() {
 	modalbg.style.display = "block";
+	heroSection.style.display = "none";
+	footer.style.display = "none";
+	return true;
 }
 
+// launch modal form
+/**
+ * A la fermeture de la modale, on fait disparaître la modale et apparaître la 
+ * section hero et le footers
+ *
+ * @return  {Boolean}
+ */
+function closeModal() {
+	modalbg.style.display = "none";
+	heroSection.style.display = "block";
+	footer.style.display = "flex";
+	return true;
+}
+
+
+
+//Exemple de fonction avec arguments : a supprimer ensuite
 /**
  * permet de tester une valeur
  *
@@ -43,7 +85,7 @@ function launchModal() {
  *
  * @return  {Boolean}           vrai ou faux
  */
-function test(argument, bidule){
-	console.log(argument);
-	return true;
-}
+// function test(argument, bidule) {
+// 	console.log(argument);
+// 	return true;
+// }
